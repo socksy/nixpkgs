5c772eb8f3cc32678f2cce45d930a73f99939b57
Ben Lovell <ben.j.lovell@gmail.com>
2022-01-22 01:17:34 +0100

    Remove default prune names
    Revert https://github.com/NixOS/nixpkgs/pull/146344 due to the fact that the defaults of this package are
    1. use findutils. ocate package
    2. set pruneNames
    which then give user a warning that pruneNames aren't supported with findutils locate, which is bizarre.

--- nixos/modules/misc/locate.nix
+++ nixos/modules/misc/locate.nix
@@ -171,9 +171,10 @@ in {
 
     pruneNames = mkOption {
       type = listOf str;
-      default = [ ".bzr" ".cache" ".git" ".hg" ".svn" ];
+      default = [];
+      example = [ ".bzr" ".cache" ".git" ".hg" ".svn" ];
       description = ''
-        Directory components which should exclude paths containing them from indexing
+        Directory components which should exclude paths containing them from indexing (when using mlocate)
       '';
     };
 
